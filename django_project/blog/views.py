17bc830fcc4a4b2f7e5a8b21c7af59bcd2d51648
liberavia <andre.gregor.herrmann@gmail.com>
2021-03-12 01:46:05 +0100

    Template inheritance and bootstrap snippet integration

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Susi Sonnenschein',
+        'title': 'Der erste Post',
+        'content': 'Der Inhalt vom ersten Post',
+        'date_posted': '21.03.2021'
+    },
+    {
+        'author': 'Josef Jedermann',
+        'title': 'Noch ein Post',
+        'content': 'Inhalt der im zweiten Post steht',
+        'date_posted': '21.03.2021'
+    },
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'title': 'Startseite',
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
+    context = {
+        'title': 'Infos',
+    }
     return render(request, 'blog/about.html')
