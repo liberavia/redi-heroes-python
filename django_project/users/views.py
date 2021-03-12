a6bc274008f60c481fe95c714a420f81ca4003f5
liberavia <andre.gregor.herrmann@gmail.com>
2021-03-12 12:41:26 +0100

    Login and Logout mechanism

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+
+
 
 def register(request):
     if request.method == 'POST':
@@ -8,9 +11,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Konto für {username} eröffnet!')
-            return redirect('blog-home')
+            messages.success(request, f'Konto für {username} eröffnet! Du kannst dich nun einloggen')
+            return redirect('login')
     else:
         form = UserRegisterForm()
-
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
