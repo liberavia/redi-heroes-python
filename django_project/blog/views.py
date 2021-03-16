f249d3f717be877a3671012b5e161b2e6ba3f483
liberavia <andre.gregor.herrmann@gmail.com>
2021-03-16 01:15:16 +0100

    Pagination and user filter

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -8,11 +9,24 @@ from django.views.generic import (
     DeleteView)
 from .models import Post
 
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 2
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/posts_user.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
