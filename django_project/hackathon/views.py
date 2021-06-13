33fa4d284b1f24b963cfc0c3fa4bfd0c65de4ba3
liberavia <andre.gregor.herrmann@gmail.com>
2021-06-13 11:49:41 +0200

    Kommentaranzeige auf Über-Seite

--- django_project/hackathon/views.py
+++ django_project/hackathon/views.py
@@ -3,14 +3,14 @@ from .models import Kommentar
 
 
 def startseite(request):
+    return render(request, 'hackathon/startseite.html')
+
+
+def ueberseite(request):
     liste_kommentare = Kommentar.objects.all()
 
     context = {
         'kommentare': liste_kommentare
     }
 
-    return render(request, 'hackathon/startseite.html', context)
-
-
-def ueberseite(request):
-    return render(request, 'hackathon/ueberseite.html')
+    return render(request, 'hackathon/ueberseite.html', context)
