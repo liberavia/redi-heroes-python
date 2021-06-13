7c30604a2fa98b5f09473ebfe550fd2788fe795d
liberavia <andre.gregor.herrmann@gmail.com>
2021-06-13 13:08:59 +0200

    Kommentar wird gespeichert

--- django_project/hackathon/views.py
+++ django_project/hackathon/views.py
@@ -6,7 +6,16 @@ def startseite(request):
     return render(request, 'hackathon/startseite.html')
 
 
+def speicher_kommentar(request):
+    neuer_autor = request.POST.get('eingabeAutor')
+    neuer_kommentartext = request.POST.get('eingabeKommentar')
+    neuer_kommentar = Kommentar(autor=neuer_autor, kommentar=neuer_kommentartext)
+    neuer_kommentar.save()
+
+
 def ueberseite(request):
+    if request.method == 'POST':
+        speicher_kommentar(request)
     liste_kommentare = Kommentar.objects.all()
 
     context = {
