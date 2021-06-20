64ac4c456f9a8a04cc8e446a0e371dc03be456b3
liberavia <andre.gregor.herrmann@gmail.com>
2021-06-20 11:41:05 +0200

    Charakter-Model hinzugefuegt

--- django_project/hackathon/admin.py
+++ django_project/hackathon/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Kommentar, Charakter
 
-# Register your models here.
+admin.site.register(Kommentar)
+admin.site.register(Charakter)
--- django_project/hackathon/models.py
+++ django_project/hackathon/models.py
@@ -7,3 +7,13 @@ class Kommentar(models.Model):
 
     def __str__(self):
         return self.autor
+
+
+class Charakter(models.Model):
+    name = models.CharField(max_length=150)
+    bild = models.CharField(max_length=255)
+    funktion = models.CharField(max_length=255, null=True)
+    familie = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
